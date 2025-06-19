2f815bd2d20e66bbf33885db039b66b1e13b3e71
RAJIT DUTTA <rajitdutta2024@gmail.com>
2025-06-19 15:54:05 +0530

    first commit

--- app/dashboard/progress/page.tsx
+++ app/dashboard/progress/page.tsx
@@ -1,5 +1,3 @@
-"use server"
-
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Progress } from "@/components/ui/progress"
